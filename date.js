5100ebe8d9ae62b5d67f5e43a01b8c1b1026197d
Praveen <praveen78720@gmail.com>
2026-09-16 10:14:18 +0530

    Add date.js file to display current date and time components

---
+++ date.js
@@ -0,0 +1,18 @@
+let date = new Date("2024-06-15 10:30:00");
+
+console.log("Year:", date.getFullYear());
+
+console.log("Date:", date.getDate());
+
+console.log("Month:", date.getMonth()+1);
+
+console.log("Day:", date.getDay());
+
+console.log("Hours:", date.getHours());
+
+console.log("Minutes:", date.getMinutes());
+
+console.log("Seconds:", date.getSeconds());
+
+
+    
